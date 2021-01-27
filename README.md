091bfa336a58e62e71aaf79c107c6bb1c61eec94
sanidhyajain1 <sanidhyajain077@gmail.com>
2021-01-27 12:25:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,144 @@
+*text-on-gif is a simple package for writing and animating text on animated as well as non-animated gifs*
+
+**Basic Usage**
+
+    const textOnGif = require("text-on-gif");
+
+    async function(){
+
+        //get gif as buffer
+        var gifBuffer = await textOnGif({
+                file_path:"/path/file.gif",
+                textMessage:"custom message"
+            });
+        console.log(gifBuffer)
+
+        //write gif as file
+        await textOnGif({
+            file_path:"/path/file.gif",
+            textMessage:"custom message",
+            write_as_file:true,
+            getAsBuffer:false,
+            write_path:"path/gif-with-text.gif"
+        });
+
+    }();
+
+# Example
+![GitHub Logo](https://i.imgur.com/1djICGN.gif)
+#
+&nbsp;
+&nbsp;
+## All properties
+Properties    | Type    | Required   | Default
+------------- | ------  | ---------- | -------
+file_path     | String  | Yes        | null
+textMessage   | String  | Not Really | ""
+font_color    | String  | No         | "white"
+font_size     | Int     | No         | 32
+alignmentX    | String  | No         | "middle"
+alignmentY    | String  | No         | "bottom"
+write_as_file | Boolean | No         | false
+write_path    | String  | No         | "/gif-with-custom-text.gif"
+getAsBuffer   | Boolean | No         | true
+font_path     | String  | No         | null
+animate       | Boolean | No         | false
+startX        | Int     | No         | null
+endX          | Int     | No         | null
+startY        | Int     | No         | null
+endY          | Int     | No         | null
+positionX     | Int     | No         | null
+positionY     | Int     | No         | null
+#
+&nbsp;
+&nbsp;  
+# file_path
+__path to a local gif file.__
+
+*URLs are not supported right now but will be added in a later version*
+#
+&nbsp;
+&nbsp;
+# textMessage
+should contain letters and numbers
+
+*if emojis and other stuff is passed then it will not be printed on the gif*
+#
+&nbsp;
+&nbsp;  
+# fonts
+### fonts available in white color
+* _**size:**_ 8
+* _**size:**_ 16
+* _**size:**_ 32
+* _**size:**_ 64
+* _**size:**_ 128
+### fonts available in black color
+* _**size:**_ 8
+* _**size:**_ 10
+* _**size:**_ 12
+* _**size:**_ 14
+* _**size:**_ 16
+* _**size:**_ 32
+* _**size:**_ 64
+* _**size:**_ 128
+#
+&nbsp;
+# alignment
+### valid values for Hozizontal Alignment
+* left
+* center
+* right
+### valid values for Vertical Alignment
+* top
+* middle
+* bottom
+#
+&nbsp;
+&nbsp;
+# write_as_file 
+set as true to write the edited gif to a local folder as a gif file
+
+if no value for **write_path** is passed and **write_as_file** is set to true then it is automatically written ot the root folder of your project with the file name "gif-with-custom-tex.gif"
+
+Writing as file overwrites previously written gifs with the same name
+#
+&nbsp;
+&nbsp;
+# write_path
+used when write_as_file is set to true 
+
+if no value is passed for **write_path** and **write_as_file** is set to true then the gif is written to the root folder of your project with the file name *"gif-with-custom-text.gif"*
+#
+&nbsp;
+&nbsp;
+# getAsBuffer
+default value is true 
+
+set to false if you dont want the file to be returned as buffer
+#
+&nbsp;
+&nbsp;
+# font_path
+path to custom font
+
+**only FNT fonts can be used**
+
+*you can use https://ttf2fnt for converting ttf fonts to fnt*
+#
+&nbsp;
+&nbsp;
+# animate
+set to true to enable text animation on gif 
+
+if set to true the values for 
+* startX
+* endX
+* startY
+* endY
+
+must be passed
+#
+&nbsp;
+&nbsp;
+# positionX
---
+++ write-on-gif.js
@@ -0,0 +1,153 @@
+"use strict";
+
+const Jimp = require("jimp");
+const { GifUtil,GifFrame,BitmapImage,GifCodec} = require('gifwrap');
+
+async function writeOnGif({file_path,textMessage,font_color,font_size,alignmentX,alignmentY,write_as_file,write_path,getAsBuffer,font_path,animate,startX,endX,startY,endY,positionX,positionY}){  
+    if(getAsBuffer==null || getAsBuffer==true || write_as_file==true){
+        if(textMessage==null){
+            textMessage="";
+        }
+        var font;
+        if(font_path==null){
+            if(font_color==null && font_size == null){
+                font = Jimp.FONT_SANS_32_WHITE
+            }else{
+                if(font_size){
+                    if(font_color=="white" || font_color==null){
+                        switch(font_size){
+                            case 8:  font = Jimp.FONT_SANS_8_WHITE;
+                            break;
+                            case 16: font = Jimp.FONT_SANS_16_WHITE;
+                            break;
+                            case 32: font = Jimp.FONT_SANS_32_WHITE;
+                            break;
+                            case 64: font = Jimp.FONT_SANS_64_WHITE;
+                            break;
+                            case 128:font = Jimp.FONT_SANS_128_WHITE;
+                            break;
+                            default:console.log("font does not exist; switched to font size 32 to prevent app from crashing"); font = Jimp.FONT_SANS_32_WHITE;
+                            break;
+                        }
+                    }else if(font_color=="black"){
+                        switch(font_size){
+                            case 8:  font = Jimp.FONT_SANS_8_BLACK;
+                            break;
+                            case 10:  font = Jimp.FONT_SANS_10_BLACK;
+                            break;
+                            case 12:  font = Jimp.FONT_SANS_12_BLACK;
+                            break;
+                            case 14:  font = Jimp.FONT_SANS_14_BLACK;
+                            break;
+                            case 16: font = Jimp.FONT_SANS_16_BLACK;
+                            break;
+                            case 32: font = Jimp.FONT_SANS_32_BLACK;
+                            break;
+                            case 64: font = Jimp.FONT_SANS_64_BLACK;
+                            break;
+                            case 128: font = Jimp.FONT_SANS_128_BLACK;
+                            break;
+                            default:console.log("font does not exist; switched to font size 32 to prevent app from crashing"); font = Jimp.FONT_SANS_32_BLACK;
+                            break;
+                        }
+                    }
+                }else if(font_color){  
+                    if(font_color=="white"){
+                        font = Jimp.FONT_SANS_32_WHITE
+                    }else if(font_color=="black"){
+                        font = Jimp.FONT_SANS_32_BLACK;
+                    }
+                }
+            }
+        }else{
+            if(font_path.substr(font_path-4)==".fnt"){
+                font = __dirname+"../../../"+font_path;
+            }else{
+                throw("fonts can only be loaded from .fnt files (https://ttf2fnt.com/ can be used to convert ttf fonts to fnt fonts)");
+            }
+        }
+    
+        if(alignmentX){
+            switch (alignmentX){
+                case "left": alignmentX = Jimp.HORIZONTAL_ALIGN_LEFT;
+                break;
+                case "middle": alignmentX = Jimp.HORIZONTAL_ALIGN_CENTER;
+                break;
+                case "right": alignmentX = Jimp.HORIZONTAL_ALIGN_RIGHT;
+                break;
+            }
+        }else{
+            alignmentX = Jimp.HORIZONTAL_ALIGN_CENTER;
+        }
+    
+        if(alignmentY){
+            switch (alignmentY){
+                case "top": alignmentY = Jimp.VERTICAL_ALIGN_TOP;
+                break;
+                case "middle": alignmentY = Jimp.VERTICAL_ALIGN_MIDDLE;
+                break;
+                case "bottom": alignmentY = Jimp.VERTICAL_ALIGN_BOTTOM;
+                break;
+            }
+        }else{
+            alignmentY = Jimp.VERTICAL_ALIGN_BOTTOM;
+        }
+    
+        var frames = [];
+        var gif;
+        await Jimp.loadFont(font).then(async function(font){
+            await GifUtil.read(__dirname+"../../../"+file_path).then(inputGif => {
+                var noOfFrames = inputGif.frames.length-1;
+                var incrementX = Math.round((endX-startX)/noOfFrames);
+                var incrementY = Math.round((endY-startY)/noOfFrames);
+                startX = startX-incrementX;
+                startY = startY-incrementY;
+                inputGif.frames.forEach(async function(frame){
+                    var jimpCopied = GifUtil.copyAsJimp(Jimp, frame);
+                    if(animate){
+                        startX = startX+incrementX;
+                        startY = startY+incrementY;
+                        jimpCopied.print(font,startX,startY,{text: textMessage},jimpCopied.bitmap.width,jimpCopied.bitmap.height-5);
+                    }else if(positionX && positionY){
+                        jimpCopied.print(font,positionX,positionY,{text: textMessage},jimpCopied.bitmap.width,jimpCopied.bitmap.height-5);
+                    }else{
+                        jimpCopied.print(font,0,0,{text: textMessage,alignmentX: alignmentX,alignmentY: alignmentY},jimpCopied.bitmap.width,jimpCopied.bitmap.height-5);
+                    }
+                    const GifCopied = new GifFrame(new BitmapImage(jimpCopied.bitmap,{
+                        disposalMethod: frame.disposalMethod,
+                        delayCentisecs: frame.delayCentisecs,
+                    }));
+                    frames.push(GifCopied);   
+                });
+            });
+            GifUtil.quantizeSorokin(frames);
+            if(write_as_file){
+                if(!write_path){
+                    write_path = __dirname+"../../../gif-with-custom-text.gif";
+                }else{
+                    if(write_path.substr(write_path.length-4)!=".gif"){
+                        write_path= write_path +".gif";
+                    }
+                }
+                GifUtil.write(write_path, frames, { loops: 0 }).then(gif => {
+                    console.log("written as file to "+write_path);
+                });
+            }
+    
+            if(getAsBuffer==null || getAsBuffer!=false){
+                const codec = new GifCodec();
+                await codec.encodeGif(frames, { loops: 0 }).then(encodedGIF => {
+                    gif = encodedGIF.buffer;
+                });
+            }else{
+                gif = "set 'getAsBuffer:true' to get gif as buffer";
+            }
+    
+        });
+        return Promise.resolve(gif);
+    }else{
+        console.log("function has no output; set 'getAsBuffer:true' to get gif as buffer or set 'write_as_file:true' to save gif as file");
+    }
+}
+
+module.exports = writeOnGif;
