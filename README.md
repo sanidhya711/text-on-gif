504aa95823923a80d8d503515e004265c0eeea47
sanidhyajain1 <sanidhyajain077@gmail.com>
2021-12-22 19:08:27 +0530

    eeeee

---
+++ README.md
@@ -0,0 +1,122 @@
+*text-on-gif is a simple package for writing and animating text on animated as well as non-animated gifs*
+
+**Basic Usage**
+```js
+    const TextOnGif = require('text-on-gif');
+
+    (async function(){
+
+        var gif = new TextOnGif({
+          file_path: "https://media0.giphy.com/media/kFgzrTt798d2w/giphy.gif"
+        });
+
+        //get as buffer
+        var buffer = await gif.textOnGif({
+            text: "text on gif sucks",
+            get_as_buffer: true
+        });
+
+        console.log(buffer);
+
+        //write as file
+        await gif.textOnGif({
+            text: "text on gif sucks",
+            get_as_buffer: false,
+            write_path: "gif-with-text.gif"
+        });
+
+    })();
+```
+## NOTE
+**DOSENT SUPPORT TRANSPERANCY**
+
+writing on gif might take some time(generally 4-5 seconds) so always use await when getting file as buffer
+
+*the amount of time taken to print the message depends on the number of frames in the gif*
+# Example
+![GitHub Logo](https://i.imgur.com/1djICGN.gif)
+#
+## All properties
+Properties    | Type    | Required   | Default
+------------- | ------  | ---------- | -------
+file_path     | String  | Yes        | null
+text_message   | String  | Not Really | ""
+font_color     | String  | No         | "white"
+font_size      | Int     | No         | 32
+alignment_x    | String  | No         | "middle"
+alignment_y   | String  | No         | "bottom"
+write_path    | String  | No         | "/gif-with-custom-text.gif"
+get_as_buffer   | Boolean | No         | true
+font_path     | String  | No         | null
+position_x     | Int     | No         | null
+position_y    | Int     | No         | null
+#
+&nbsp;
+&nbsp;  
+# file_path
+___path to a local gif file___ or ___URL___
+
+&nbsp;
+&nbsp;
+# text_message
+should contain letters and numbers
+
+*if emojis and other stuff is passed then it will not be printed on the gif*
+
+&nbsp;
+# alignment
+### valid values for Hozizontal Alignment
+* left
+* center
+* right
+### valid values for Vertical Alignment
+* top
+* middle
+* bottom
+#
+&nbsp;
+&nbsp;
+# write_path
+if value is passed for **write_path** then the gif is written to the root folder of your project with the specified file name
+#
+&nbsp;
+&nbsp;
+# get_as_buffer
+default value is true 
+
+set to false if you dont want the file to be returned as buffer
+#
+&nbsp;
+&nbsp;
+# font_path
+path to custom font
+
+**only FNT fonts can be used**
+DOSENT WORK WITH VERSION 2 UPDATE INNA FEW DAYS
+*you can use https://ttf2fnt for converting ttf fonts to fnt*
+#
+&nbsp;
+&nbsp;
+# position_x
+position of text on the x axis
+
+must be a int
+
+*now support percentage values*
+#
+&nbsp;
+&nbsp;
+# position_y
+position of text on the y axis
+
+must be a int
+
+*now supports percentage values*
+#
+&nbsp;
+&nbsp;
+# Things which will be fixed in the upcoming versions
+* will support transperancy
+* multi line text will be aligned properly
+* more user friendly 
+* make readme better XD
---
+++ text-on-gif.js
@@ -0,0 +1,205 @@
+const gifFrames = require('gif-frames');
+const GIFEncoder = require('gif-encoder-2')
+const fs = require('fs');
+const Canvas = require('canvas');
+const Image = Canvas.Image;
+const createCanvas = Canvas.createCanvas;
+const EventEmitter = require('events').EventEmitter;
+
+class TextOnGif extends EventEmitter{
+
+    #file_path;
+    #width;
+    #height;
+
+
+    constructor(
+        {
+            file_path,
+            font_style,
+            font_color,
+            font_size,
+            alignment_x,
+            alignment_y,
+            offset_x,
+            offset_y,
+            position_x,
+            position_y,
+            font_path
+        }
+    ){
+        super();
+        this.#file_path = file_path;
+        this.font_style = font_style || "chaloops";
+        this.font_color = font_color || "black";
+        this.font_size = font_size || "32px";
+        this.alignment_x = alignment_x || "center";
+        this.alignment_y = alignment_y || "bottom";
+        this.offset_x = offset_x || 10;
+        this.offset_y = offset_y || 10;
+        this.position_x = position_x;
+        this.position_y = position_y;
+        this.font_path = font_path;
+
+        this.#height = null;
+        this.#width = null;
+
+        this.extractedFrames = [];
+        this.extractionComplete = false;
+
+        this.#extractFrames();
+    }
+
+    
+    async #extractFrames(){
+
+        var frameData = await gifFrames({url: this.#file_path,frames: 'all',outputType: 'jpg',cumulative: true});
+
+        this.#width = frameData[0].frameInfo.width;
+        this.#height = frameData[0].frameInfo.height;
+
+        for (let index = 0; index < frameData.length; index++) {
+
+            await new Promise((resolve,reject)=>{
+
+                const image = new Image();
+
+                image.onload = () => {
+                    this.extractedFrames.push({
+                        imageData: image,
+                        delay: frameData[index].frameInfo.delay * 10,
+                        disposal: frameData[index].frameInfo.disposal
+                    });
+                    resolve();
+                }
+
+                image.src = frameData[index].getImage()._obj;
+                    
+            });
+    
+        }
+
+        this.extractionComplete = true;
+        this.emit('extraction complete');
+
+    }
+
+    async #writeMessage(text,get_as_buffer,write_path){
+        const encoder = new GIFEncoder(this.#width,this.#height,'neuquant',false,this.extractedFrames.length);
+        encoder.setRepeat(0);
+
+        const canvas = createCanvas(this.#width,this.#height);
+        const ctx = canvas.getContext('2d',{alpha: false});
+
+        ctx.fillStyle = this.font_color;
+        ctx.font = this.font_size + ' ' + this.font_style;
+
+        if(write_path && !get_as_buffer){
+            const writeStream = fs.createWriteStream(write_path);
+            encoder.createReadStream().pipe(writeStream);
+        }
+
+        encoder.start();
+
+        encoder.on('progress', percent => {
+            this.emit("progress",percent);
+        });
+
+        if(this.position_x){
+            ctx.textAlign = "start";
+            var x = this.position_x;
+        }else{
+            if(this.alignment_x == "right"){
+                ctx.textAlign = "right";
+                var x = this.#width - this.offset_x;
+            }else if(this.alignment_x == "left"){
+                ctx.textAlign = "left";
+                var x = this.offset_x;
+            }else{
+                ctx.textAlign = "center";
+                var x = this.#width / 2;
+            }
+        }
+
+        
+        if(this.position_y){
+            ctx.textBaseline = "alphabetic";
+            var y = this.position_y;
+        }else{
+            if(this.alignment_y == "top"){
+                ctx.textBaseline = "hanging";
+                var y = this.offset_y;
+            }else if(this.alignment_y == "middle"){
+                ctx.textBaseline = "middle";
+                var y = this.height/2;
+            }else{
+                ctx.textBaseline = "alphabetic";
+                var y = this.#height - this.offset_y
+            }
+        }
+            
+        for(let index = 0; index < this.extractedFrames.length; index++) {
+            ctx.drawImage(this.extractedFrames[index].imageData, 0, 0);
+            ctx.fillText(text,x,y);
+            encoder.setDelay(this.extractedFrames[index].delay);
+            encoder.setDispose(this.extractedFrames[index].disposal);
+            encoder.addFrame(ctx);
+        }
+
+        encoder.finish();
+
+        this.emit("finished");
+
+        if(get_as_buffer && write_path){
+            await new Promise((resolve,reject)=>{
+                fs.writeFile(write_path, encoder.out.getData(), error => {
+                    resolve();
+                });
+            })
+        }
+
+        if(get_as_buffer){
+            return Promise.resolve(encoder.out.getData());
+        }else{
+            return null;
+        }
+    }
+
+    async textOnGif({text,get_as_buffer,write_path}){
+        if(!write_path && !get_as_buffer){
+            get_as_buffer = true;
+        }
+
+        var buffer = null;
+
+        if(this.extractionComplete){
+            buffer = await this.#writeMessage(text,get_as_buffer,write_path);
+        }else{
+            await new Promise((resolve,reject)=>{
+                this.on('extraction complete',async()=>{
+                    buffer = await this.#writeMessage(text,get_as_buffer,write_path);
+                    resolve();
+                });
+            });
+        }
+
+        if(buffer){
+            return Promise.resolve(buffer);
+        }
+    }
+
+    get width(){
+        return this.#width;
+    }
+
+    get height(){
+        return this.#height;
+    }
+
+    get file_path(){
+        return this.#file_path;
+    }
+
+}
+
+module.exports = TextOnGif;
